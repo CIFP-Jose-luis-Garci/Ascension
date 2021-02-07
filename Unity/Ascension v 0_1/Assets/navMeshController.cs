58e48855abf121cda617768078eea7fd345be39a
Riko218 <71703409+Riko218@users.noreply.github.com>
2021-02-07 18:53:25 +0100

    Pruebas de Ia en otra scenea aparte.
    Comenzamos con las pruebas del señor de seguridad.

---
+++ Unity/Ascension v 0_1/Assets/navMeshController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class navMeshController : MonoBehaviour
+{
+
+    public Transform target; //Empty que seguira nuestro objetivo
+    private NavMeshAgent agente;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agente = GetComponent<NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agente.destination = target.position;
+        if(agente.destination == target.position)
+        {
+            anim.SetBool("Parado", true);
+            anim.SetBool("Vigilando", false);
+            }
+        }
+    }
+
