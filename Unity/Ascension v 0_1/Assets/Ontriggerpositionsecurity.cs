3837073be588392f5b1c87e4ab0e63b62c662e97
Riko218 <71703409+Riko218@users.noreply.github.com>
2021-02-10 21:28:23 +0100

    Seguimiento del guardia de seguridad y parado y vuelta a continuar
    Todo preparado para le movimiento del personaje solo habrá que animar el cubo que se quiera para las rutas y ajustar tiempos.

---
+++ Unity/Ascension v 0_1/Assets/Ontriggerpositionsecurity.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ontriggerpositionsecurity : MonoBehaviour
+{
+    public Animator anim;
+    public GameObject agente;
+
+    private void Start()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "enemigo")
+        {
+            anim.SetBool("Parado", true);
+            anim.SetBool("Vigilando", false);
+        }  
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "enemigo")
+        {
+            anim.SetBool("Parado", false);
+            anim.SetBool("Vigilando", true);
+        }
+    }
+           
+}
+
--- Unity/Ascension v 0_1/Assets/navMeshController.cs
+++ Unity/Ascension v 0_1/Assets/navMeshController.cs
@@ -6,27 +6,23 @@ using UnityEngine.AI;
 
 public class navMeshController : MonoBehaviour
 {
-
+    private Animator anim;
     public Transform target; //Empty que seguira nuestro objetivo
     private NavMeshAgent agente;
-    private Animator anim;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         agente = GetComponent<NavMeshAgent>();
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         agente.destination = target.position;
-        if(agente.destination == target.position)
-        {
-            anim.SetBool("Parado", true);
-            anim.SetBool("Vigilando", false);
-            }
-        }
     }
-
+}
