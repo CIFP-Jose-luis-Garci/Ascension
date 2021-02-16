220ee710241c69fb0eaac0387f54c2d57ec8b580
Riko218 <71703409+Riko218@users.noreply.github.com>
2021-02-16 09:01:42 +0100

    Modificando el alpha para que cuando entres a el juego escena empiece difuminando

--- Unity/Ascension v 0_1/Assets/Scripts/DifuminadoIntro.cs
+++ Unity/Ascension v 0_1/Assets/Scripts/DifuminadoIntro.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DifuminadoIntro : MonoBehaviour
 {
+    public float alphaLevel = 256f;
+    public GameObject Image;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
+    
+    
     void Update()
     {
-        
+        alphaLevel--;
+        Image.GetComponent<Image>().color = new Color(1, 1, 1, alphaLevel) * Time.deltaTime / 5;
     }
 }
