8d5b3cafb484ddd9dc88388c1226778f346a63e6
mariulxd <73118224+mariulxd@users.noreply.github.com>
2021-02-18 17:14:08 +0100

    musica Demo1

---
+++ Unity/Ascension v 0_1/Assets/Scripts/NivelMusic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NivelMusic : MonoBehaviour
+{
+    public GameObject clip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        clip = GameObject.FindGameObjectWithTag("clip_audio");
+        clip.GetComponent<AudioSource>().Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
