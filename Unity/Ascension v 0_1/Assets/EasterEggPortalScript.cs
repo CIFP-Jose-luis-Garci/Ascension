5d930bf4b270d96876fe336b5731d40c126bda52
TheTibus <72516070+TheTibus@users.noreply.github.com>
2021-02-21 21:42:16 +0100

    Añadido el despacho y pequeño Easter Egg de Portal.

---
+++ Unity/Ascension v 0_1/Assets/EasterEggPortalScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasterEggPortalScript : MonoBehaviour
+{
+    public GameObject eastereggPortal;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        eastereggPortal.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider target)
+
+        {
+            
+            if(target.gameObject.tag == "Player")
+            {
+             eastereggPortal.SetActive(true);
+            }
+        }         
+void OnTriggerExit(Collider target)
+
+        {
+            
+            if(target.gameObject.tag == "Player")
+            {
+             eastereggPortal.SetActive(false);
+            }
+        }         
+
+        
+
+}
