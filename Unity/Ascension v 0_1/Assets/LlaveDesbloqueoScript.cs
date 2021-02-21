1ee5e9bceff0182d73c2cc0d5b5c1e0c079f2815
TheTibus <72516070+TheTibus@users.noreply.github.com>
2021-02-22 00:33:19 +0100

    Jugabilidad Primera Planta Acabada 100%
    Hay que revisar las cámaras y un poco el gameplay para poder tener ya la primera parte del juego jugable.

---
+++ Unity/Ascension v 0_1/Assets/LlaveDesbloqueoScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LlaveDesbloqueoScript : MonoBehaviour
+{
+
+    public GameObject bloqueopuerta;
+    public GameObject luztarjeta;
+    public MeshRenderer tarjeta;
+    public SphereCollider tarjetacollider;
+    public AudioSource audioSource;
+    public AudioClip sonidorecoleccion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider target)
+
+        {
+            //Relacionado en la tarjeta.
+            tarjeta.enabled = false;
+            audioSource.PlayOneShot(sonidorecoleccion, 1f);
+            luztarjeta.SetActive(false);
+            tarjetacollider.enabled = false;
+            
+            //Bloqueo de puerta.
+            
+            bloqueopuerta.SetActive(false);
+
+            //Sonido recolección.
+
+            audioSource.PlayOneShot(sonidorecoleccion, 0.2f);
+
+        }  
+
+}
