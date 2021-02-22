56b6a1760ef0d14e8e3f1584882c04d0441442ff
LauraLlamas <laurallamasclase@gmail.com>
2021-02-22 18:02:45 +0100

    Logos tarjetas HUD

--- Unity/Ascension v 0_1/Assets/LlaveDesbloqueoScript.cs
+++ Unity/Ascension v 0_1/Assets/LlaveDesbloqueoScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LlaveDesbloqueoScript : MonoBehaviour
 {
@@ -11,11 +12,14 @@ public class LlaveDesbloqueoScript : MonoBehaviour
     public SphereCollider tarjetacollider;
     public AudioSource audioSource;
     public AudioClip sonidorecoleccion;
+    [SerializeField] Image LogoTarjeta;
+   
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        LogoTarjeta.enabled = false;
+      
     }
 
     // Update is called once per frame
@@ -32,6 +36,7 @@ public class LlaveDesbloqueoScript : MonoBehaviour
             audioSource.PlayOneShot(sonidorecoleccion, 1f);
             luztarjeta.SetActive(false);
             tarjetacollider.enabled = false;
+            LogoTarjeta.enabled = true;
             
             //Bloqueo de puerta.
             
