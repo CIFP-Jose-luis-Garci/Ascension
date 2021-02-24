8d20d1d7aab2991fdf0857cd1593ddf152a6608b
TheTibus <72516070+TheTibus@users.noreply.github.com>
2021-02-24 21:02:10 +0100

    SeguirTrabajandoEnCasa

---
+++ Unity/Ascension v 0_1/Assets/Scripts/DesbloqueoPuertaServers.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesbloqueoPuertaServers : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] Animator animacionPuerta;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip sonidoreactivadorpuerta;
+    [SerializeField] GameObject LuzAbiertaServer;
+    [SerializeField] GameObject LuzCerradaServer;
+    private bool estadentroServer;
+    void Start()
+    {
+        animacionPuerta.SetBool("PuertaAbierta", false);
+        LuzAbiertaServer.SetActive(false);
+        LuzCerradaServer.SetActive(true);
+        estadentroServer = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("X") && estadentroServer == true)
+        {
+           animacionPuerta.SetBool("PuertaAbierta", true);
+            print("funciono");
+            audioSource.PlayOneShot(sonidoreactivadorpuerta, 0.2f);
+            LuzAbiertaServer.SetActive(true);
+            LuzCerradaServer.SetActive(false);
+        }
+        print(estadentroServer);
+    }
+
+    void OnTriggerEnter(Collider Server)
+
+    {
+        print("Estas entrando en el server");
+        estadentroServer = false;
+
+    }
+    void OnTriggerExit(Collider Server)
+
+    {
+        estadentroServer = false;
+
+    }
+
+}
---
+++ Unity/Ascension v 0_1/Assets/Scripts/PuertaDespacho.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuertaDespacho : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] GameObject bloqueopuerta;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip sonidoreactivadorpuerta;
+    [SerializeField] GameObject LuzAbiertaDespachos;
+    [SerializeField] GameObject LuzCerradaDespachos;
+    private bool estadentro;
+    void Start()
+    {
+        LuzAbiertaDespachos.SetActive(false);
+        LuzCerradaDespachos.SetActive(true);
+        estadentro = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("X") && estadentro == true)
+        {
+            bloqueopuerta.SetActive(false);
+            print("funciono");
+            audioSource.PlayOneShot(sonidoreactivadorpuerta, 0.2f);
+            LuzAbiertaDespachos.SetActive(true);
+            LuzCerradaDespachos.SetActive(false);
+        }
+
+    }
+
+    void OnTriggerEnter(Collider Despacho)
+
+    {
+        print("Estas entrando en el despacho");
+        estadentro = true;
+
+    }
+    void OnTriggerExit(Collider Despacho)
+
+    {
+        estadentro = false;
+
+    }
+
+
+}
