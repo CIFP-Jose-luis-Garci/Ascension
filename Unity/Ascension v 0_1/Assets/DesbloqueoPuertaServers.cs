3929e6170fbbb8e1d5a222af7364ac11531046e8
TheTibus <72516070+TheTibus@users.noreply.github.com>
2021-02-24 18:57:03 +0100

    Avances en jugabilidad del segundo piso.

---
+++ Unity/Ascension v 0_1/Assets/DesbloqueoPuertaServers.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesbloqueoPuertaServers : MonoBehaviour
+{
+    [SerializeField] GameObject bloqueopuerta;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip sonidoreactivadorpuerta;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerStay(Collider target)
+
+    {
+        if (Input.GetButtonDown("X"))
+        {
+            bloqueopuerta.SetActive(false);
+            print("funciono");
+            audioSource.PlayOneShot(sonidoreactivadorpuerta, 0.2f);
+        }
+
+    }
+}
