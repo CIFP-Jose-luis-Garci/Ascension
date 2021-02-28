e946280306fa7c067c875bae93061d04bb2ab4d2
TheTibus <72516070+TheTibus@users.noreply.github.com>
2021-02-28 23:14:03 +0100

    Introducción por código de los sonidos de Andrés.

--- Unity/Ascension v 0_1/Assets/Scripts/ServerScript.cs
+++ Unity/Ascension v 0_1/Assets/Scripts/ServerScript.cs
@@ -11,18 +11,25 @@ public class ServerScript : MonoBehaviour
     public int segundos;
     [SerializeField] GameObject FinalDelNivel;
     [SerializeField] Animator animatorPuertaCerrada;
+    [SerializeField] Animator luzServer;
+
     [SerializeField] Image usb;
     [SerializeField] GameObject USBAnim;
+    [SerializeField] AudioClip startServer;
+    [SerializeField] AudioClip serverAliniciar;
+    [SerializeField] AudioClip serverSuccess;
+    [SerializeField] AudioSource audioSource;
     public float prueba;
 
     // Start is called before the first frame update
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         USBAnim.SetActive(false);
-       estaRangoServer = false;
-       FinalDelNivel.SetActive(false);
-       animatorPuertaCerrada.SetBool("PuertaCuarentena", false);
-       usb.enabled = false;
+        estaRangoServer = false;
+        FinalDelNivel.SetActive(false);
+        animatorPuertaCerrada.SetBool("PuertaCuarentena", false);
+        usb.enabled = false;
     }
 
     // Update is called once per frame
@@ -68,6 +75,9 @@ public class ServerScript : MonoBehaviour
         if(Input.GetButtonDown("X") && estaRangoServer == true && segundos == 0)  
         {
            StartCoroutine("CuentaAtras");
+           audioSource.PlayOneShot(startServer, 0.7f);
+            audioSource.PlayOneShot(serverAliniciar, 1f);
+            luzServer.SetTrigger("ServerOn");
            USBAnim.SetActive(true);
         }
     }  
@@ -79,6 +89,8 @@ public class ServerScript : MonoBehaviour
         {
             FinalDelNivel.SetActive(true);
             animatorPuertaCerrada.SetBool("PuertaCuarentena", false);
+            
+
             usb.enabled = true;
             USBAnim.SetActive(false);
         }
@@ -99,7 +111,7 @@ public class ServerScript : MonoBehaviour
             animatorPuertaCerrada.SetBool("PuertaCuarentena", true);
             yield return new WaitForSeconds(0.0001f);
         }
-
+        audioSource.PlayOneShot(serverSuccess, 1f);
     }
 
   
---
+++ Unity/Ascension v 0_1/Assets/Scripts/SonidosServer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonidosServer : MonoBehaviour
+{
+    public GameObject audioServers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         audioServers.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider target)
+
+        {
+            
+            if(target.gameObject.tag == "Player")
+            {
+              audioServers.SetActive(true);
+            }
+        }         
+void OnTriggerExit(Collider target)
+
+        {
+            
+            if(target.gameObject.tag == "Player")
+            {
+              audioServers.SetActive(false);
+            }
+        }         
+}
